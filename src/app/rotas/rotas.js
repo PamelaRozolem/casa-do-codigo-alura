9e434f9cbb4ebdfe330eafdf19a024cc5a518981
pam <pamelarozolem@gmail.com>
2018-12-30 19:47:50 -0200

    aula 2

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -7,7 +7,7 @@ module.exports = (app) =>{
                     <meta charset="utf-8">
                 </head>
                 <body>
-                    <h1>Casa do Cádigo</h1>
+                    <h1>Casa do Código</h1>
                 </body>
             </html>
         `
@@ -15,19 +15,23 @@ module.exports = (app) =>{
     });
     
     app.get("/livros", function(req,res){
-        res.send(
-            `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1>Listagem de Livros</h1>
-                </body>
-            </html>
-        `
+        res.marko(
+          require('../views/livros/lista/lista.marko'),
+          {
+              livros:[
+                  {
+                      id:1,
+                      titulo:'Fundamentos do node'
+                  },
+                  {
+                      id:2,
+                      titulo:'Node avançado'
+                  }
+              ]
+
+          }
         );
-    })
+    });
     
 };
 
